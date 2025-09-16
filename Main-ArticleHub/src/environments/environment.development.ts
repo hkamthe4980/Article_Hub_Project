e835d7a60ab3e7f6f2a5885dc0238163c51ef31e
hkamthe4980 <hkamthe8@gmail.com>
2025-09-16 19:06:01 +0530

    rename index html file

--- Main-ArticleHub/src/environments/environment.development.ts
+++ Main-ArticleHub/src/environments/environment.development.ts
@@ -2,3 +2,7 @@ export const environment = {
     production: false,
     apiUrl: 'http://localhost:8080'
 };
+// export const environment = {
+//     production: false,
+//     apiUrl: 'https://article-hub-project-vequ.vercel.app/'
+// };
