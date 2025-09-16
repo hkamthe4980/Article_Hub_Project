47a4ff31a6264aadbfad3e56688a4fda21d05d7f
hkamthe4980 <hkamthe8@gmail.com>
2025-09-16 17:13:51 +0530

    show server running msg

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -3,6 +3,8 @@ const http = require('http');
 const app = require('./index');
 
 
-
+app.get('/',(req,resp)=>{
+    resp.send("Server is running");
+});
 const server = http.createServer(app);
 server.listen(process.env.PORT);
